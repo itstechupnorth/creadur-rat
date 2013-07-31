899c8622122f76e9345a9dc6dcb924c76ce2c9c6
Manuel Suárez Sánchez <ssm767@gmail.com>
2013-07-31 21:03:49 +0200

    Convert TMF854LicenseHeader to use LicenseFamily constructor

--- apache-rat-core/src/main/java/org/apache/rat/api/MetaData.java
+++ apache-rat-core/src/main/java/org/apache/rat/api/MetaData.java
@@ -68,7 +68,6 @@ public class MetaData {
     public static final String RAT_LICENSE_FAMILY_CATEGORY_VALUE_UNKNOWN =
             "?????";
     public static final String RAT_LICENSE_FAMILY_CATEGORY_VALUE_DOJO = "DOJO ";
-    public static final String RAT_LICENSE_FAMILY_CATEGORY_VALUE_TMF = "TMF  ";
 
     public static final Datum RAT_LICENSE_FAMILY_CATEGORY_DATUM_GEN =
             new Datum(RAT_URL_LICENSE_FAMILY_CATEGORY,
@@ -79,21 +78,17 @@ public class MetaData {
     public static final Datum RAT_LICENSE_FAMILY_CATEGORY_DATUM_DOJO =
             new Datum(RAT_URL_LICENSE_FAMILY_CATEGORY,
                     RAT_LICENSE_FAMILY_CATEGORY_VALUE_DOJO);
-    public static final Datum RAT_LICENSE_FAMILY_CATEGORY_DATUM_TMF =
-            new Datum(RAT_URL_LICENSE_FAMILY_CATEGORY,
-                    RAT_LICENSE_FAMILY_CATEGORY_VALUE_TMF);
 
     // License Family Standard Names
     public static final String RAT_URL_LICENSE_FAMILY_NAME = RAT_BASE_URL
             + "#LicenseFamilyName";
-    public static final String RAT_LICENSE_FAMILY_NAME_VALUE_MODIFIED_BSD_LICENSE =
-            "Modified BSD License";
+	public static final String RAT_LICENSE_FAMILY_NAME_VALUE_MODIFIED_BSD_LICENSE = "Modified BSD License";
     public static final String RAT_LICENSE_FAMILY_NAME_VALUE_ACADEMIC_FREE_LICENSE_VERSION_2_1 =
             "Academic Free License, Version 2.1";
     public static final String RAT_LICENSE_FAMILY_NAME_VALUE_UNKNOWN = "?????";
-    public static final Datum RAT_LICENSE_FAMILY_NAME_DATUM_MODIFIED_BSD_LICENSE =
-            new Datum(RAT_URL_LICENSE_FAMILY_NAME,
-                    RAT_LICENSE_FAMILY_NAME_VALUE_MODIFIED_BSD_LICENSE);
+	public static final Datum RAT_LICENSE_FAMILY_NAME_DATUM_MODIFIED_BSD_LICENSE = new Datum(
+			RAT_URL_LICENSE_FAMILY_NAME,
+			RAT_LICENSE_FAMILY_NAME_VALUE_MODIFIED_BSD_LICENSE);
     public static final Datum RAT_LICENSE_FAMILY_NAME_DATUM_ACADEMIC_FREE_LICENSE_VERSION_2_1 =
             new Datum(RAT_URL_LICENSE_FAMILY_NAME,
                     RAT_LICENSE_FAMILY_NAME_VALUE_ACADEMIC_FREE_LICENSE_VERSION_2_1);
